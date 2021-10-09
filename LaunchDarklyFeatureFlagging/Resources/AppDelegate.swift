d51c7c82711ab4cb2d5bf872d7e751bd8f8251a9
codedubu <mccaine.river@gmail.com>
2021-10-08 21:11:15 -0700

    Adds functional Feature Flag that changes labels based on dashboard switch being activated.

--- LaunchDarklyFeatureFlagging/Resources/AppDelegate.swift
+++ LaunchDarklyFeatureFlagging/Resources/AppDelegate.swift
@@ -6,31 +6,45 @@
 //
 
 import UIKit
+import LaunchDarkly
+
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    private let mobileKey = "mob-648573d3-b60d-40b6-8fda-5ca43201aef7"
+    
+    private func setUpLDCClient() {
+        let user = LDUser(key: "mccaine.river@gmail.com")
+        
+        var config = LDConfig(mobileKey: mobileKey)
+        config.eventFlushInterval = 30.0
+        LDClient.start(config: config, user: user)
+    }
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setUpLDCClient()
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
---
+++ LaunchDarklyFeatureFlagging/Screens/MainTestVC.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  LaunchDarklyFeatureFlagging
+//
+//  Created by River McCaine on 10/8/21.
+//
+
+import UIKit
+import LaunchDarkly
+
+class MainTestVC: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var featureFlagTestLabel: UILabel!
+    @IBOutlet weak var currentFlagStatusLabel: UILabel!
+    
+    // MARK: - Properties
+    fileprivate let featureFlagKey = "change-text-of-test"
+    
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        LDClient.get()?.observe(key: featureFlagKey, owner: self) { [weak self] changedFlag in
+            self?.featureFlagDidUpdate(changedFlag.key)
+        }
+        
+        checkFeatureValue()
+    }
+    
+    
+    // MARK: - Helpers
+    fileprivate func checkFeatureValue() {
+        var featureFlagValue = LDClient.get()!.variation(forKey: featureFlagKey, defaultValue: false)
+        
+        if featureFlagValue == false {
+            updateTestLabel(value: featureFlagValue)
+            updateCurrentFlagStatusLabel(value: featureFlagValue)
+        } else {
+            updateTestLabel(value: featureFlagValue)
+            updateCurrentFlagStatusLabel(value: featureFlagValue)
+        }
+        
+    }
+    
+    
+    fileprivate func updateTestLabel(value:Bool) {
+        if value == false {
+            featureFlagTestLabel.text = "This label is currently in the default state."
+        } else {
+            featureFlagTestLabel.text = "LaunchDarkly's Feature Flag has been activated."
+        }
+    }
+    
+
+    fileprivate func updateCurrentFlagStatusLabel(value:Bool) {
+        if value == false {
+            currentFlagStatusLabel.text = "Current Flag Status: \n ❌ False ❌"
+        } else {
+            currentFlagStatusLabel.text = "Current Flag Status: \n ✅ True ✅"
+        }
+    }
+ 
+    
+    func featureFlagDidUpdate(_ key: LDFlagKey) {
+        if key == featureFlagKey {
+            checkFeatureValue()
+        }
+    }
+    
+} // END OF CLASS
+
