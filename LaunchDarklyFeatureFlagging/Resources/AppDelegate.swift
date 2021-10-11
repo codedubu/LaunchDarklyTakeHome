9b7a3bc43cb02fa19aaf4204ec8d4828f94d497f
codedubu <mccaine.river@gmail.com>
2021-10-10 23:43:52 -0700

    Initial commit.

--- LaunchDarklyFeatureFlagging/Resources/AppDelegate.swift
+++ LaunchDarklyFeatureFlagging/Resources/AppDelegate.swift
@@ -38,4 +38,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 } // END OF CLASS
-
