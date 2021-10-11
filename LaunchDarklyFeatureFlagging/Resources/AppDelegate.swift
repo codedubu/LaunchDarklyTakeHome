c64065f9a3f08b92d9dfe4a1e36e279e12d0e4b8
codedubu <mccaine.river@gmail.com>
2021-10-11 10:47:41 -0700

    Adds production key for usage.

--- LaunchDarklyFeatureFlagging/Resources/AppDelegate.swift
+++ LaunchDarklyFeatureFlagging/Resources/AppDelegate.swift
@@ -11,13 +11,14 @@ import LaunchDarkly
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    private let mobileKey = "mob-648573d3-b60d-40b6-8fda-5ca43201aef7"
+    private let mobileTestKey = "mob-648573d3-b60d-40b6-8fda-5ca43201aef7"
+    private let mobileProdKey =  "mob-c4291fc6-2174-490b-8d3a-e7bc92ad54dd"
     
     
     private func setUpLDCClient() {
         let user = LDUser(key: "mccaine.river@gmail.com")
         
-        var config = LDConfig(mobileKey: mobileKey)
+        var config = LDConfig(mobileKey: mobileProdKey)
         config.eventFlushInterval = 30.0
         LDClient.start(config: config, user: user)
     }
